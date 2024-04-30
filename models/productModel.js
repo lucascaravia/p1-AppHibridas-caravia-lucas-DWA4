5f6d3cc202d17028b03c553f12c058bb72757e7b
lucascaravia <88347319+lucascaravia@users.noreply.github.com>
2024-04-30 10:37:24 -0300

    se elimino productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,14 +1,1 @@
-// Modelo de Producto
-import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    stock: Number
-})
-
-
-const Product = mongoose.model('Product', productSchema);
-// Exporto el Objeto Product
-
-export default Product
